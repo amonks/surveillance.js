abc23e51a87ffe24064d7573b00ffd86fa69475c
Andrew Monks <a@monks.co>
2017-06-16 16:42:32 -0500

    hanlde stops in pipe

--- src/pipeline/pipe.js
+++ src/pipeline/pipe.js
@@ -1,6 +1,17 @@
-const pipe = (...fns) => {
-  const composedFns = fns.reverse().reduce((fn, prev) => v => fn(prev(v)))
-  return data => composedFns(data)
+import stop from './stop'
+
+export const pipe = (...fns) => {
+  const composedFns = fns.reverse().reduce((fn, prev) => {
+    if (prev === stop) return v => stop
+
+    return v => {
+      const last = prev(v)
+      if (last === stop) return stop
+      return fn(last)
+    }
+  })
+
+  return composedFns
 }
 
 export default pipe
