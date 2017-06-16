c798d987de63d7e07fe6ef23178e97a50b05eb24
Andrew Monks <a@monks.co>
2017-06-16 17:53:18 -0500

    docs: document mergeWith

--- src/pipeline/mergeWith.js
+++ src/pipeline/mergeWith.js
@@ -1,3 +1,11 @@
+/**
+ * ### surveillance.pipeline.mergeWith
+ *
+ * Object -> (...Object) -> Object
+ *
+ * Merges a bunch of objects with a given object
+ */
+
 const mergeWith = (target = {}) => datas =>
   datas.reduce((a, b) => ({ ...a, ...b }), target)
 
