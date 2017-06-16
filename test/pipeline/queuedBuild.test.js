6a99a878476d8133e11e0e3a55dc18e5def2cb2b
Andrew Monks <a@monks.co>
2017-06-16 16:33:11 -0500

    test elements added _after_ the queued-period

--- test/pipeline/queuedBuild.test.js
+++ test/pipeline/queuedBuild.test.js
@@ -19,4 +19,21 @@ describe('surveillance.pipeline.queuedBuild', () => {
 
     expect(results).toEqual(['later-event', 'later-event-2'])
   })
+
+  it('continues working _after_ the pipeline is ready', async () => {
+    const pipeline = surveillance.pipeline.queuedBuild({
+      pipeline: [
+        async conf => {
+          await sleep(200)
+          return arg => `later-${arg}`
+        },
+      ],
+    })
+
+    await sleep(500)
+
+    const results = await Promise.all([pipeline('event'), pipeline('event-2')])
+
+    expect(results).toEqual(['later-event', 'later-event-2'])
+  })
 })
