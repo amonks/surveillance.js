ce3570d2bc8158f2835018ff5c0e826def4f2a7f
Andrew Monks <a@monks.co>
2017-06-16 17:56:34 -0500

    docs: document addTimestamp

--- src/annotators/addTimestamp.js
+++ src/annotators/addTimestamp.js
@@ -1,3 +1,11 @@
+/**
+ * ### addTimestamp
+ *
+ * () -> Object -> Object
+ *
+ * addTimestamp takes an object and adds a timestamp property to it. The property is called 'timestamp'.
+ */
+
 const addTimestamp = e => ({
   ...e,
   timestamp: Date.now(),
