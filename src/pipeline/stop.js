70b99330227270837040dd1ac93d170260f21bf9
Andrew Monks <a@monks.co>
2017-06-16 17:53:40 -0500

    docs: document stop

--- src/pipeline/stop.js
+++ src/pipeline/stop.js
@@ -1,1 +1,13 @@
-export default `___$$$-none-${Math.random()}-$$$___`
+/**
+ * ### surveillance.pipeline.stop
+ *
+ * String
+ *
+ * If a pipeline function returns this value, it skips the rest of the pipeline.
+ *
+ * If a pipeline function _is_ this value, it skips the rest of the pipeline.
+ *
+ * Use it kinda like a Maybe monad, except without the manual overhead.
+ */
+
+export default `___$$$-stop-${Math.random()}-$$$___`
