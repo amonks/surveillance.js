a39eec1a06d1a678cce40cf167702ba5b173bb1b
Andrew Monks <a@monks.co>
2017-06-16 17:57:03 -0500

    BREAKING CHANGE: addTimestamp takes an (empty) config object
    I think I'll avoid breaking changes in the long run by saying "all
    pipeline elements take a config object"

--- src/annotators/addTimestamp.js
+++ src/annotators/addTimestamp.js
@@ -6,7 +6,7 @@
  * addTimestamp takes an object and adds a timestamp property to it. The property is called 'timestamp'.
  */
 
-const addTimestamp = e => ({
+const addTimestamp = () => e => ({
   ...e,
   timestamp: Date.now(),
 })
--- test/annotators/addTimestamp.test.js
+++ test/annotators/addTimestamp.test.js
@@ -5,7 +5,7 @@ import * as surveillance from '../../src'
 describe('surveillance/annotators/addTimestamp', () => {
   it('adds a timestamp to an object', () => {
     const obj = { a: 1 }
-    const out = surveillance.annotators.addTimestamp(obj)
+    const out = surveillance.annotators.addTimestamp()(obj)
     expect(Object.keys(out).sort()).toEqual(['a', 'timestamp'].sort())
   })
 })
