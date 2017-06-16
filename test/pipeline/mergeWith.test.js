fc7416a884be4f78d66e978bec196c5e71854551
Andrew Monks <a@monks.co>
2017-06-16 16:33:00 -0500

    add empty-object-merge test

--- test/pipeline/mergeWith.test.js
+++ test/pipeline/mergeWith.test.js
@@ -12,4 +12,14 @@ describe('surveillance.pipeline.mergeWith', () => {
       ]),
     ).toEqual({ a: 1, b: 2, c: 3, d: 4 })
   })
+
+  it('merges its event inputs given an empty configuration object', () => {
+    expect(
+      surveillance.pipeline.mergeWith()([
+        { a: 3, b: 3, c: 3 },
+        { a: 2, b: 2 },
+        { a: 1 },
+      ]),
+    ).toEqual({ a: 1, b: 2, c: 3 })
+  })
 })
