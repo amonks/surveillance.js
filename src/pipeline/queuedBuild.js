3b440b36d9c3582ffecbb3d9ae2e1249c4890f38
Andrew Monks <a@monks.co>
2017-06-16 17:53:33 -0500

    docs: document queuedBuild

--- src/pipeline/queuedBuild.js
+++ src/pipeline/queuedBuild.js
@@ -2,6 +2,14 @@ import makeQueue from '../util/makeQueue'
 
 import build from './build'
 
+/**
+ * ### surveillance.pipeline.queuedBuild
+ *
+ * conf -> Object -> Object
+ *
+ * like build, but returns immediately and enqueues events until the pipeline is built
+ */
+
 const queuedBuild = conf => makeQueue(build(conf))
 
 export default queuedBuild
