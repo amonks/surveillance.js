908da4fb967921286c12f24b2767ee314898e76e
Andrew Monks <a@monks.co>
2017-06-16 16:33:39 -0500

    test logger options

--- test/sinks/log.test.js
+++ test/sinks/log.test.js
@@ -16,6 +16,22 @@ describe('surveillance.sinks.log', () => {
     expect(res).toBe('event')
   })
 
+  it('works with a custom logger', () => {
+    const fn = jest.fn()
+    const output = surveillance.sinks.log({ logger: fn })('event')
+    expect(output).toBe('event')
+    expect(fn.mock.calls).toEqual([['surveillance data: "event"']])
+  })
+
+  it('works with a custom message', () => {
+    const fn = jest.fn()
+    const output = surveillance.sinks.log({ logger: fn, message: 'message' })(
+      'event',
+    )
+    expect(output).toBe('event')
+    expect(fn.mock.calls).toEqual([['message: "event"']])
+  })
+
   it('logs to the console', () => {
     surveillance.sinks.log()('event')
     expect(console.log).toHaveBeenCalledWith('surveillance data: "event"')
