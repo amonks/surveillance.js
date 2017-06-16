d5adbaa17b27ee80ca0261c209a45edffb0fe422
Andrew Monks <a@monks.co>
2017-06-16 17:53:53 -0500

    docs: document log

--- src/sinks/log.js
+++ src/sinks/log.js
@@ -1,3 +1,15 @@
+/**
+ * ### surveillance.sinks.log
+ *
+ * ({ logger: function, message: String }) -> Object -> Object
+ *
+ * logs a message and an object with the given logger, returning the object.
+ *
+ * the default logger is 'console.log.bind(console)'.
+ *
+ * the default message is 'surveillance data'.
+ */
+
 const log = (config = {}) => {
   const logger = config.logger || console.log.bind(console)
   const message = config.message || 'surveillance data'
