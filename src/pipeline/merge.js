51a5fd8709edd3ada9a75dd95cc7496797cf8f2e
Andrew Monks <a@monks.co>
2017-06-16 17:53:12 -0500

    docs: document merge

--- src/pipeline/merge.js
+++ src/pipeline/merge.js
@@ -1,5 +1,13 @@
 import mergeWith from './mergeWith'
 
+/**
+ * ### surveillance.pipeline.merge
+ *
+ * (...Object) -> Object
+ *
+ * takes a bunch of objects and merges them together
+ */
+
 const merge = mergeWith({})
 
 export default merge
