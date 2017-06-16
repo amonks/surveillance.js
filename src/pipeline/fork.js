03c0582b124a7b01ced68e686058d1a67124a779
Andrew Monks <a@monks.co>
2017-06-16 17:53:06 -0500

    docs: document fork

--- src/pipeline/fork.js
+++ src/pipeline/fork.js
@@ -1,3 +1,11 @@
+/**
+ * ### surveillance.pipeline.fork
+ *
+ * (...function) -> Object -> [Object]
+ *
+ * splits a pipeline through multiple functions
+ */
+
 const fork = (...fns) => data => Promise.all(fns.map(f => f(data)))
 
 export default fork
