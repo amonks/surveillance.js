d8189f713dd4077c01614fb77759926673750992
Andrew Monks <a@monks.co>
2017-06-17 14:56:54 -0500

    feat: google analytics events pass through
    also document google analytics sink a bit

--- src/sinks/googleAnalytics/index.js
+++ src/sinks/googleAnalytics/index.js
@@ -1,10 +1,17 @@
 import isogram from './isogram'
 
+/**
+ * ### surveillance.sinks.googleAnalytics
+ *
+ * ({ trackingId: string }) => Object => Object
+ */
+
 const ga = ({ trackingId }) => {
   isogram(trackingId)
 
   const track = data => {
     window.ga('send', data)
+    return data
   }
 
   return track
